15b5dbe862354c0385c14f1e388180c5a68bbf31
brianliamgrant <49319266+brianliamgrant@users.noreply.github.com>
2019-11-24 18:25:40 -0500

    Add files via upload

---
+++ Final Project 4630/Inventory1.sql
@@ -0,0 +1,6 @@
+INSERT INTO pizza.Inventory (inventoryID, quantity, item)
+VALUES (1, 100, 'Pepperoni Pizza'),
+(2, 100, 'Meat Lovers Pizza'),
+(3, 100, 'Cheese Pizza'),
+(4, 100, 'Vegan Pizza'),
+(5, 100, 'Buffalo Chicken Pizza');
---
+++ Final Project 4630/menu1.sql
@@ -0,0 +1,6 @@
+INSERT INTO pizza.Menu (menuID, Inventory_inventoryID, price)
+VALUES (1, 1, 10.99),
+(2, 2, 9.99),
+(3, 3, 11.99),
+(4, 4, 12.99),
+(5, 5, 13.99);
