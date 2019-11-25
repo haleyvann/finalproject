e3b13584a4d72fb4a70c1c0a71eaf2b70773bbcb
tc30605 <49877588+tc30605@users.noreply.github.com>
2019-11-24 21:03:25 -0500

    Update RegisterServlet.java

--- Final Project 4630/src/controllers/RegisterServlet.java
+++ Final Project 4630/src/controllers/RegisterServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.NewUser;
+import model.Customer;
 import dbhelpers.Register;
 
 /**
