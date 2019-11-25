e00286e2a5b94ab1ba836d1d720ddb56c8ce7563
brianliamgrant <49319266+brianliamgrant@users.noreply.github.com>
2019-11-24 19:35:42 -0500

    Update MenuServlet.java

--- Final Project 4630/src/controllers/MenuServlet.java
+++ Final Project 4630/src/controllers/MenuServlet.java
@@ -1,11 +1,17 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Menu;
 
 /**
  * Servlet implementation class MenuServlet
@@ -34,8 +40,54 @@ public class MenuServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession session = request.getSession();
+		
+		// default jsp 
+		String url = "/index.jsp";
+		
+		String cart = "";
+		
+		// setup the selected menu item and update cart depending on which submit button was pressed
+		if(request.getParameter("addPep") != null) {
+			// pass to menu page
+			url = "/menu.jsp";
+			// setup pep pizza to pass to the cart
+			Menu selectedPep = new Menu(1, "Pepperoni Pizza", 10.99);
+			cart = selectedPep.toString();
+		} else if(request.getParameter("addCheese") != null) {
+			// pass to menu page
+			url = "/menu.jsp";
+			// setup selected cheese pizza to pass to the cart
+			Menu selectedCheese = new Menu(2, "Cheese Pizza", 9.99);
+			cart = selectedCheese.toString();
+		} else if(request.getParameter("addVegan") != null) {
+			// pass to menu page
+			url = "/menu.jsp";
+			// setup selected vegan pizza to pass to the cart
+			Menu selectedVegan = new Menu(3, "Vegan Pizza", 11.99);
+			cart = selectedVegan.toString();
+		} else if(request.getParameter("addML") != null) {
+			// pass to menu page
+			url = "/menu.jsp";
+			// setup selected ml pizza to pass to the cart
+			Menu selectedML = new Menu(4, "Meat Lovers Pizza", 12.99);
+			cart = selectedML.toString();
+		} else if(request.getParameter("addBC") != null) {
+			url = "/menu.jsp";
+			// setup selected bc pizza to pass to the cart
+			Menu selectedBC = new Menu(5, "Buffalo Chicken Pizza", 13.99);
+			cart = selectedBC.toString();
+		} else if(request.getParameter("cart") != null) {
+			url = "/cart.jsp";
+			// setup selected bc pizza to pass to the cart
+		} 
+		 
+		// send cart information to the menu.jsp and attach it to the session
+		session.setAttribute("cart", cart);
+		
+		  // send control to the next component
+		  RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		  dispatcher.forward(request, response);
 	}
 
 }
