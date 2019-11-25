2ce1e3ff1c3cbb4f27efdc6007aa07c6ea00d8ee
brianliamgrant <49319266+brianliamgrant@users.noreply.github.com>
2019-11-25 13:34:28 -0500

    Update Order.java

--- Final Project 4630/Order.java
+++ Final Project 4630/Order.java
@@ -13,8 +13,8 @@ public class Order extends Menu {
 	//information regarding one order
 	private int orderID = 0;
 	private String orderDate;
-	private int orderQuantity;
-	private double revenue;
+	private int orderQuantity = 0;
+	private double revenue = 0;
 	
 	/**
 	 * @param orderDate
@@ -22,11 +22,9 @@ public class Order extends Menu {
 	 * @param orderQuantity
 	 * @param orderPrice
 	 */
-	public Order(int orderID, String orderDate, int orderQuantity, double revenue) {
+	public Order(int orderID, String orderDate) {
 		this.orderID += orderID;
 		this.orderDate = orderDate;
-		this.orderQuantity = orderQuantity;
-		this.revenue = revenue;
 	}
 
 	
@@ -86,6 +84,11 @@ public class Order extends Menu {
 	public void setOrderQuantity(int orderQuantity) {
 		this.orderQuantity = orderQuantity;
 	}
+	
+	public void addToRevenue(double price, int quantity) {
+		this.orderQuantity += quantity;
+		this.revenue += revenue + (price * quantity);
+	}
 
 
 
@@ -101,8 +104,8 @@ public class Order extends Menu {
 	@Override
 	public String toString() {
 		//TODO toString method that returns the menu item(s), quantity and the total price of the order
-		return "Order [orderDate=" + orderDate + ", orderQuantity=" + orderQuantity + ", revenue=" + revenue
-				+ "]";
+		return "<p>" + "<ul>" + "<li>" + "Time of order: " + orderDate + "</li>" + "<li>" + "Number of items in order: " 
+				+ orderQuantity + "</li>" + "<li>" + "Total order price: $" + getRevenue() + "</li>" + "</ul>" + "</p>";
 	}
 	
 }
