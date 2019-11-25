ebc0a100cb85af207d8fb81d2b37f11175fcd420
tc30605 <49877588+tc30605@users.noreply.github.com>
2019-11-24 20:18:31 -0500

    Update LoginServlet.java

--- Final Project 4630/src/controllers/LoginServlet.java
+++ Final Project 4630/src/controllers/LoginServlet.java
@@ -1,11 +1,14 @@
 package controllers;
 
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.User;
+import dbhelpers.Validate;
 
 /**
  * Servlet implementation class LoginServlet
@@ -26,16 +29,33 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String url = "/index.jsp";
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		Validate valid = new Validate("netappsdb", "root", "pwd");
+        
+        String email = request.getParameter("email");
+        String pass = request.getParameter("pass");
+        
+        
+        if(valid.checkUser(email, pass)){
+            RequestDispatcher rs = request.getRequestDispatcher("/index.jsp");
+            rs.forward(request, response);
+        }
+        else{
+          // out.println("Username or Password incorrect");
+           RequestDispatcher rs = request.getRequestDispatcher("/login.jsp");
+           rs.forward(request, response);
+        }
+		
 	}
 
 }
